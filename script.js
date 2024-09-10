0555b68f7286ae19c342a6fa9ad26ab395d5159d
benhidayat <98013492+Benhidayat@users.noreply.github.com>
2024-09-11 01:47:47 +0700

    created the look and styled the weather app

---
+++ script.js
@@ -0,0 +1,14 @@
+// const description = document.querySelector('.desc');
+
+// async function getWeatherData() {
+//     const API__ID = 'RHYGAUUEU9DTPZYDZQXUMP25V';
+//     const url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/';
+
+//     const response = await fetch(url + 'indonesia/?key=' + API__ID)
+//     const weatherData = await response.json();
+
+//     console.log(weatherData);
+//     // description.textContent = weatherData.currentConditions.icon;
+// }
+
+// getWeatherData();
